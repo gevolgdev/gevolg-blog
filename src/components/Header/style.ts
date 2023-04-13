899a787788785b29dc1f22196884e2862682fe18
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-13 13:24:08 -0300

    Header: Ajuste design

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  width: 1200px;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 auto;
-  padding: 25px 0;
+  padding: 45px 0;
 
   .title {
+    font-size: 1.5rem;
     text-transform: uppercase;
     font-weight: 900;
     
@@ -21,7 +21,7 @@ export const HeaderContainer = styled.header`
 `;
 
 export const DashboardButton = styled(Link)`
-  width: 150px;
+  width: 140px;
   display: flex;
   justify-content: center;
   padding: 10px 0;
@@ -30,9 +30,10 @@ export const DashboardButton = styled(Link)`
   background: #22c55e;
   border: 2px solid #22c55e;
   color: white;
+  font-size: .9rem;
 `
 export const FeedButton = styled(Link)`
-  width: 150px;
+  width: 140px;
   display: flex;
   justify-content: center;
   padding: 10px 0;
@@ -40,4 +41,5 @@ export const FeedButton = styled(Link)`
   border-radius: 7px;
   border: 2px solid #1e1e1e;
   color: #1e1e1e;
+  font-size: .9rem;
 `
