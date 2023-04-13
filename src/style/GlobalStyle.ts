bcbfab5e1dbc679450443255b9c98ccbb5f9be17
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-12 23:22:58 -0300

    Global Style

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {padding: 0; margin: 0; box-sizing: border-box};
+
+  body {
+    font-size: 16px;
+    color: #1e1e1e;
+    font-family: 'Inter', sans-serif;
+    background: #E3E3E3;
+  }
+`;
