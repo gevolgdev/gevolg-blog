058fd27a3cef77332bc8c7b7fdec4ed9cd7c12c2
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-13 13:23:44 -0300

    Dasboard: Style e coleta de dados dos inputs

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,11 +1,80 @@
+import { useRef, useState, ChangeEvent } from 'react';
+
 import { Header } from "../../components";
+import { DashboardContainer, DashboardContent } from "./style";
+import { BsImage, BsFillFileEarmarkCheckFill } from 'react-icons/bs';
+
+interface ArticleProps {
+  subject: string;
+  image: string;
+  title: string;
+  articleBody: string;
+};
 
 const Dashboard: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const INITIAL_ARTICLE: ArticleProps = {
+    subject: '',
+    image: '',
+    title: '',
+    articleBody: '',
+  };
+  const [article, setArticle] = useState<ArticleProps>(INITIAL_ARTICLE);
+
+  const handleSaveImage = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.files) {
+      const imageFile = event.target.files[0];
+      const imageURL = URL.createObjectURL(imageFile);
+      setArticle(prev => ({...prev, image: imageURL}));
+    };
+  };
+
+  const handleSaveForms = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { id, value} = event.target;
+    setArticle(prev => ({...prev, [id]: value}))
+  }
+
   return (
-    <>
+    <DashboardContainer>
       <Header feed/>
-      <div>Dashboard</div>
-    </>
+
+      <DashboardContent>
+        <h1>Novo Artigo</h1>
+        <div className="forms-container">
+
+          <div className="flex-row">
+            <div className="input-container">
+              <span>Assunto</span>
+              <input id='subject' type='text' onChange={handleSaveForms}/>
+            </div>
+            <div className="input-container">
+              <span>Imagem</span>
+              <button onClick={() => inputRef.current?.click()} className='button-file'>
+                { article.image 
+                ? <><BsFillFileEarmarkCheckFill style={{marginRight: 5}}/> Imagem selecionada!</>
+                : <><BsImage style={{marginRight: 5}}/> Selecione uma imagem</>
+                }
+              </button>
+              <input ref={inputRef} onChange={handleSaveImage} type='file' style={{display: 'none'}}/>
+            </div>
+          </div>
+
+          <div className="input-container">
+            <span>Título</span>
+            <input id="title" type='text' onChange={handleSaveForms}/>
+          </div>
+
+          <div className="input-container">
+            <span>Artigo</span>
+            <textarea id='articleBody' onChange={handleSaveForms}/>
+          </div>
+
+          <button className="post">Publicar</button>
+        </div>
+      </DashboardContent>
+
+    </DashboardContainer>
   )
 }
 
---
+++ src/routes/Dashboard/style.ts
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const DashboardContainer = styled.section`
+  width: 1200px;
+  margin: 0 auto;
+`
+
+export const DashboardContent = styled.div`
+  margin: 0 auto;
+  padding: 30px 0 150px;
+  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  .forms-container {
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 20px;
+
+    .flex-row {
+      display: flex;
+      flex-direction: row;
+      gap: 15px;
+    }
+    .input-container {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    }
+
+    input {
+      display: flex;
+      padding: 15px 10px;
+      font-size: 1rem;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      font-family: 'Inter', sans-serif;
+      &:focus {
+        outline: 1px solid #4e4e4e;
+      }
+    }
+    .button-file {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: white;
+      padding: 15px 10px;
+      font-size: 1rem;
+      color: #8e8e8e;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      &:focus {
+        outline: 1px solid #4e4e4e;
+      }
+    }
+    .button-file-on {
+      background: #bbf7d0;
+    }
+    textarea {
+      height: 250px;
+      display: flex;
+      padding: 15px 10px;
+      font-size: 1rem;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      resize: none;
+      font-family: 'Inter', sans-serif;
+      &:focus {
+        outline: 1px solid #4e4e4e;
+      }
+    }
+    .post {
+      background: #22c55e;
+      border: none;
+      color: white;
+      padding: 15px 0;
+      border-radius: 7px;
+      font-size: 1rem;
+      cursor: pointer;
+    }
+  }
+`
