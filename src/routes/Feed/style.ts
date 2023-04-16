200cbea06706483e4b025c25935bcc8dd9f21c0c
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-16 18:42:17 -0300

    Feed: Design inicial

---
+++ src/routes/Feed/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FeedContainer = styled.section`
+  width: 1128px;
+  margin: 0 auto;
+`
+
+export const FeedContent = styled.div`
+  width: 900px;
+  margin: 0 auto;
+`
