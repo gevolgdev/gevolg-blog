c4c5e522168216e9efe76c62668d0f4f6b223192
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-16 18:44:08 -0300

    Feed: Retornando os artigos

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -1,11 +1,24 @@
-import { Header } from "../../components"
+import { Header, ArticleCard} from "../../components"
+import { FeedContainer, FeedContent } from "./style";
+
+import { useSelector } from "react-redux";
+import { useArticle } from "../../redux/sliceArticles";
+import { ArticleProps } from "../../types/types";
 
 const Feed: React.FC = () => {
+
+  const articles: ArticleProps[] = useSelector(useArticle);
+  const newArticles: ArticleProps[] = articles.slice(1)
+
   return (
-    <>
+    <FeedContainer>
       <Header dashboard/>
-      <div>Feed</div>
-    </>
+      <FeedContent>
+        {newArticles.map(item => (
+          <ArticleCard article={item}/>
+        ))}
+      </FeedContent>
+    </FeedContainer>
   )
 }
 
